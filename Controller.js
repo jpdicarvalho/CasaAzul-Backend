29b25570b320a0f33fa1f62b8079b111ab46ab4f
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-05-15 09:02:37 -0300

    create route /api/AddNewPatient/

--- Controller.js
+++ Controller.js
@@ -8,6 +8,9 @@ import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
 
 // Create the connection to the database mysql on PlanetScale
 const db = mysql.createConnection(process.env.DATABASE_URL)
@@ -20,6 +23,23 @@ db.connect((error) => {
     console.log('Conexão bem-sucedida ao banco de dados!');
   }
 });
+
+app.post('/api/AddNewPatient/', (req, res) =>{
+ 
+    const newName = req.body.newName;
+    const newDateBirth = req.body.newDateBirth;
+    const newCEP = req.body.newCEP;
+    const newStreet = req.body.newStreet;
+    const newNumber = req.body.newNumber;
+    const newBairro = req.body.newBairro;
+    const newCity = req.body.newCity;
+    const newDateCreation = req.body.newDateCreation;
+    const hasLaudo = req.body.hasLaudo;
+    const newCID = req.body.newCID;
+
+  console.log(newName, newDateBirth)
+})
+
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
