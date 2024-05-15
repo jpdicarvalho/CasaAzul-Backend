c1b48b552bab40d5b9bdb76ff0ece430463148d1
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2024-05-14 21:52:33 -0300

    Create conection with db

--- Controller.js
+++ Controller.js
@@ -1,10 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from "body-parser";
+import mysql from "mysql2";
+
 import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+// Create the connection to the database mysql on PlanetScale
+const db = mysql.createConnection(process.env.DATABASE_URL)
+
+// Verify connection to the database
+db.connect((error) => {
+  if (error) {
+    console.error('Erro ao conectar ao banco de dados:', error.message);
+  } else {
+    console.log('Conexão bem-sucedida ao banco de dados!');
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
